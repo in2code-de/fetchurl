d63192ac753fc65bf2d34e03f710cf52423a4166
Marcus Schwemer <marcus.schwemer@in2code.de>
2016-12-22 15:38:48 +0100

    [TASK] Cleanup ext_tables.php
    Move direct modifications of $GLOBALS['TCA'] to
    Configuration/Overrides/TCA/tt_content.php

--- ext_tables.php
+++ ext_tables.php
@@ -3,22 +3,3 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-/**
- * Include Plugins
- */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('fetchurl', 'Pi1', 'Fetchurl');
-
-/**
- * Include Flexform
- */
-// Pi1
-$TCA['tt_content']['types']['list']['subtypes_addlist']['fetchurl_pi1'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'fetchurl_pi1',
-    'FILE:EXT:fetchurl/Configuration/FlexForm/FlexFormPi1.xml'
-);
-
-/**
- * Disable non needed fields in tt_content
- */
-$TCA['tt_content']['types']['list']['subtypes_excludelist']['fetchurl_pi1'] = 'select_key,pages,recursive';
