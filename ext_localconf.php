e3a7719ac31338ec148bf2c6895bf1d411ffa245
Alex Kellner <alexander.kellner@in2code.de>
2020-02-27 10:58:39 +0100

    [TASK] Add new extension icon

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,25 +3,27 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'In2code.fetchurl',
-    'Pi1',
-    [
-        'Fetch' => 'fetch,iframe'
-    ],
-    [
-        'Fetch' => ''
-    ]
-);
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'In2code.fetchurl',
+        'Pi1',
+        [
+            'Fetch' => 'fetch,iframe'
+        ],
+        [
+            'Fetch' => ''
+        ]
+    );
 
-/**
- * Register the extension icon
- * @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry
- */
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon('extensions-fetchurl-wizard',
-	'TYPO3\\CMS\\Core\\Imaging\\IconProvider\\BitmapIconProvider',
-	[
-		'source' => 'EXT:fetchurl/ext_icon.gif',
-	]
-);
+    /**
+     * Register icons
+     */
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'extensions-fetchurl-wizard',
+        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
+        [
+            'source' => 'EXT:fetchurl/Resources/Public/Icons/Extension.svg',
+        ]
+    );
+});
